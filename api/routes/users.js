f50c42b2fe7bc861509093555f545b15de60d542
huynhmai <huynhmai305@gmail.com>
2019-07-04 12:02:34 +0700

    update

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,11 +3,16 @@ var router = express.Router();
 const User = require('../models/user');
 const Bill = require('../models/bill')
 
+/* GET users listing. */
+router.get('/profile', (req, res, next) => {
+  User.findOne({
+    where: {
       username: req.query.name
     }
   })
   .then(result => {
     res.sendStatus(200).send(result)
+    console.log(result)
   })
   .catch( err => res.json({
     msg: 'Không tìm thấy người dùng'
