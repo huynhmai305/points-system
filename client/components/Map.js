02ca1c3f4107e116ae478e0ff1e2b09f82079ce4
huynhmai <huynhmai305@gmail.com>
2019-10-11 17:46:47 +0700

    update map, get user location

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,28 +1,51 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import React, { Component } from "react";
-import MapGL from "react-map-gl";
+import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import DeckGL, { GeoJsonLayer } from "deck.gl";
 import Geocoder from "react-map-gl-geocoder";
+import { Button } from 'reactstrap'
 
 // Please be a decent human and don't abuse my Mapbox API token.
 // If you fork this sandbox, replace my API token with your own.
 // Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
 const MAPBOX_TOKEN =
-    "pk.eyJ1Ijoic21peWFrYXdhIiwiYSI6ImNqcGM0d3U4bTB6dWwzcW04ZHRsbHl0ZWoifQ.X9cvdajtPbs9JDMG-CMDsA";
-
-export default class App extends Component {
-    state = {
-        viewport: {
-            width: 400,
-            height: 400,
-            latitude: 37.7577,
-            longitude: -122.4376,
-            zoom: 8
-        },
-        searchResultLayer: null
-    };
-
+    "pk.eyJ1IjoiaHV5bmhtYWkiLCJhIjoiY2sxbG1sbmNjMDdiOTNibW1yc2NhM2RnaSJ9.Q_5TfOAujayDMAKafDBEqw";
+export default class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                width: "100vw",
+                height: "100vh",
+                latitude: 42.430472,
+                longitude: -123.334102,
+                zoom: 16
+            },
+            searchResultLayer: null,
+            userLocation: {}
+        };
+    }
     mapRef = React.createRef();
+
+    setUserLocation = () => {
+        navigator.geolocation.getCurrentPosition(position => {
+            let setUserLocation = {
+                lat: position.coords.latitude,
+                long: position.coords.longitude
+            };
+            let newViewport = {
+                height: "100vh",
+                width: "100vw",
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                zoom: 10
+            }
+            this.setState({
+                viewport: newViewport,
+                userLocation: setUserLocation
+            })
+        })
+    }
 
     componentDidMount() {
         window.addEventListener("resize", this.resize);
@@ -49,7 +72,6 @@ export default class App extends Component {
     // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
     handleGeocoderViewportChange = viewport => {
         const geocoderDefaultOverrides = { transitionDuration: 1000 };
-
         return this.handleViewportChange({
             ...viewport,
             ...geocoderDefaultOverrides
@@ -64,30 +86,47 @@ export default class App extends Component {
                 getFillColor: [255, 0, 0, 128],
                 getRadius: 1000,
                 pointRadiusMinPixels: 10,
-                pointRadiusMaxPixels: 10
+                pointRadiusMaxPixels: 10,
             })
         });
     };
 
     render() {
         const { viewport, searchResultLayer } = this.state;
-
         return (
-            <MapGL
-                ref={this.mapRef}
-                {...viewport}
-                onViewportChange={this.handleViewportChange}
-                mapboxApiAccessToken={MAPBOX_TOKEN}
-            >
-                <Geocoder
-                    mapRef={this.mapRef}
-                    onResult={this.handleOnResult}
-                    onViewportChange={this.handleGeocoderViewportChange}
-                    mapboxApiAccessToken={MAPBOX_TOKEN}
-                    position="top-left"
-                />
-                <DeckGL {...viewport} layers={[searchResultLayer]} />
-            </MapGL>
+            <div>
+                <div className="map">
+                    <ReactMapGL
+                        ref={this.mapRef}
+                        {...viewport}
+                        onViewportChange={this.handleViewportChange}
+                        mapboxApiAccessToken={MAPBOX_TOKEN}
+                        mapStyle="mapbox://styles/mapbox/streets-v11"
+                    >
+                        <img src="/static/images/search_location.png" onClick={this.setUserLocation} className="location-btn"/>
+                        <Geocoder
+                            mapRef={this.mapRef}
+                            onResult={this.handleOnResult}
+                            onViewportChange={this.handleGeocoderViewportChange}
+                            mapboxApiAccessToken={MAPBOX_TOKEN}
+                            position="top-left"
+                        />
+                        {Object.keys(this.state.userLocation).length !== 0 ? (
+                            <Marker
+                                latitude={this.state.userLocation.lat}
+                                longitude={this.state.userLocation.long}
+                            >
+                                <div className="location-icon">
+                                    <img src="/static/images/location.png"/>
+                                </div>
+                            </Marker>
+                        ) : (
+                                <div>Empty</div>
+                            )}
+                        {/* <DeckGL {...viewport} layers={[searchResultLayer]} /> */}
+                    </ReactMapGL>
+                </div>
+            </div>
         );
     }
 }
