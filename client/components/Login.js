14cb67d1966dc986d860179ad7d9ec75789e5e43
huynhmai <huynhmai305@gmail.com>
2019-12-10 11:44:14 +0700

    update validate with react-validation validator

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,102 +1,89 @@
 import React, { Component } from 'react';
-import { Container, Col, Form, FormGroup, Label, Input, Button, FormFeedback, Toast,ToastHeader,ToastBody, Spinner} from 'reactstrap';
+import { Container, Col, FormGroup, Label, Button, FormFeedback, Toast,ToastHeader,ToastBody, Spinner} from 'reactstrap';
 import Router from 'next/router';
 import Swal from 'sweetalert2'
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+import { isEmail, isEmpty } from 'validator';
+
+const required = (value) => {
+  if (isEmpty(value)) {
+      return <small className="form-text text-danger">Vui lòng nhập thông tin</small>;
+  }
+}
+
+const validateEmail = (value) => {
+  if (!isEmail(value)) {
+    return <small className="form-text text-danger">{value} không phải email hợp lệ</small>;
+  }
+}
+
+const maxLength = (value) => {
+  if (value.toString().trim().length > 9) {
+    return <small className="form-text text-danger">Password không vượt quá 9 kí tự</small>;
+  }
+}
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
-      password: '',
-      validate: {
-        emailState: '',
-      },
-      msg: '',
-      msg_err:'',
-      item: [],
-      show:false,
-      colortoast:''
+      password: ''
     }
     this.handleChange = this.handleChange.bind(this);
-    this.toggle = this.toggle.bind(this)
-  }
-
-  validateEmail(e) {
-    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const { validate } = this.state
-    if (emailRex.test(e.target.value)) {
-      validate.emailState = 'has-success'
-    } else {
-      validate.emailState = 'has-danger'
-    }
-    this.setState({ validate })
   }
 
   handleChange = (event) => {
-    const { target } = event;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const { name } = target;
+    const { name, value } = event.target;
     this.setState({
       [name]: value,
     });
   }
-  toggle(){
-    this.setState({show: false})
-  }
+
   submitForm(e) {
     e.preventDefault();
-    console.log(`Email: ${this.state.email} va password: ${this.state.password}`)
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password
-      })
-    })
-      .then(response => response.json())
-      .then(item => {
-        Swal.fire(`Đăng nhập thành công`,"", "success")
-        .then (rs => {
-          localStorage.setItem('user', JSON.stringify(item))
-          let role = item[0].role;
-          if (role === 0) {
-            Router.push('/admin')
-          } else if (role === 1) {
-            Router.push('/store')
-          } else {
-            Router.push('/user')
-          }
+    this.form.validateAll();
+    if ( this.checkBtn.context._errors.length === 0 ) {
+      console.log(`Email: ${this.state.email} va password: ${this.state.password}`)
+      fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password
         })
       })
-      .catch(err => {
-        console.log(err)
-        // this.setState({
-        //   msg_err:'Vui lòng kiểm tra lại email, password',
-        //   colortoast:'danger',
-        //   show:false
-        // });
-        Swal.fire("Thất bại","Vui lòng kiểm tra lại email, password","error")
-    })
+        .then(response => response.json())
+        .then(item => {
+          Swal.fire(`Đăng nhập thành công`,"", "success")
+          .then (rs => {
+            localStorage.setItem('user', JSON.stringify(item))
+            let role = item[0].role;
+            if (role === 0) {
+              Router.push('/admin')
+            } else if (role === 1) {
+              Router.push('/store')
+            } else {
+              Router.push('/user')
+            }
+          })
+        })
+        .catch(err => {
+          console.log(err)
+          Swal.fire("Thất bại","Vui lòng kiểm tra lại email, password","error")
+        })
+    }
   }
 
   render() {
     const { email, password } = this.state;
     return (
       <Container className="App">
-        <Toast isOpen={this.state.show}>
-          <ToastHeader icon={this.state.colortoast} toggle={this.toggle}>
-            Thông báo
-          </ToastHeader>
-          <ToastBody>
-          {this.state.msg} {'  '}
-          <Spinner color={this.state.colortoast} />
-          </ToastBody>
-        </Toast>
-        <Form className="form" onSubmit={(e) => this.submitForm(e)} method="POST">
+        <Form className="form" onSubmit={(e) => this.submitForm(e)} ref={c => { this.form = c }}>
           <Col>
             <FormGroup>
               <Label>Email</Label>
@@ -106,20 +93,11 @@ class Login extends Component {
                 id="exampleEmail"
                 placeholder="myemail@email.com"
                 value={email}
-                valid={this.state.validate.emailState === 'has-success'}
-                invalid={this.state.validate.emailState === 'has-danger'}
+                validations={[required, validateEmail]}
                 onChange={(e) => {
-                  this.validateEmail(e)
                   this.handleChange(e)
                 }}
-                required
               />
-              <FormFeedback valid>
-                Nhập email thành công
-              </FormFeedback>
-              <FormFeedback>
-                Vui lòng nhập một email chính xác
-              </FormFeedback>
             </FormGroup>
           </Col>
           <Col>
@@ -132,21 +110,15 @@ class Login extends Component {
                 placeholder="********"
                 value={password}
                 onChange={(e) => this.handleChange(e)}
-                required
+                validations={[required, maxLength]}
               />
-              <FormFeedback valid>
-                Nhập password thành công
-              </FormFeedback>
-              <FormFeedback>
-                Password không được trống
-              </FormFeedback>
             </FormGroup>
           </Col>
           <FormGroup>
             <Button color="success" type="submit">
               Đăng nhập
             </Button>
-            <div className="text-danger mt-3">{this.state.msg_err}</div>
+            <CheckButton style={{ display: 'none' }} ref={c => { this.checkBtn = c }} />
           </FormGroup>
         </Form>
       </Container>
